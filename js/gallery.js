8f752bb3afa52b5c86c1a7bf3857abfb649c2437
Ann Maria Jose <annmariajose1412@gmail.com>
2024-02-07 21:08:23 -0600

    Add gallery lightbox script

--- js/gallery.js
+++ js/gallery.js
@@ -139,3 +139,28 @@ function closeInfoBox() {
     //Hide all visible descriptions
     hideAllDescriptions();
 }
+
+$(document).ready(function(){
+
+    /* Open lightbox on button click */
+    $('main #gallery #album img').click(function(){
+        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+        $('.box').fadeIn();
+
+        //Check if lightbox has an image and clear if true
+        if ($('.box').contents('img')) {
+            $('.box').contents().remove('img');
+        }
+
+        var img = $(this).clone(); //Duplicate DOM element
+        $('.box').append(img); //Insert duplicated element in another element
+    });
+
+    /* Click to close lightbox */
+    $('.close, .backdrop').click(function(){
+        $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+            $('.backdrop').css('display', 'none');
+        });
+        $('.box').fadeOut();
+    });
+});
